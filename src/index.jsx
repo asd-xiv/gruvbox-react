541101b4590cbc797bbfe8f37d0b40e78ebbca1f
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-03-07 19:22:08 +0100

    fix: Forgot export of useTheme redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,4 @@
 export { Input } from "./input/input"
 export { Button } from "./button/button"
+export { useTheme } from "./use-theme/theme.hook"
+export * as useThemeRedux from "./use-theme/theme.redux"
